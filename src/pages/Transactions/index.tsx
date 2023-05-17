015dbd7d199ea5ea7281adf086fdf324d652c8c2
Lucas <lucasrafael123araujolima@gmail.com>
2023-05-17 07:41:24 -0400

    feat: :sparkles: Componentização

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,40 @@
 import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de website</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>12/01/2023</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Big Mac</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 5,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>12/01/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
